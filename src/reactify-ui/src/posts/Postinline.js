8c0294aa8007a28586f26a135c1425c5e26c543c
hupratt <hp@hp.com>
2019-04-21 13:33:35 +0200

    barebone post display

--- src/reactify-ui/src/posts/Postinline.js
+++ src/reactify-ui/src/posts/Postinline.js
@@ -3,21 +3,7 @@ import React, { Component } from "react";
 class PostInline extends Component {
   render() {
     const { post } = this.props;
-    const { elClass } = this.props;
-    const showContent = elClass === "card" ? "d-block" : "d-none";
-    return (
-      <div>
-        {post !== undefined ? (
-          <div className={elClass}>
-            <h1 />
-
-            <p className={showContent}>{post.content}</p>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
+    return <div>{post == undefined ? "" : <p>{post.content}</p>}</div>;
   }
 }
 
